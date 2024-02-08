dc5643b83bc1917a4301a3e7b7eeb96341a32067
Lautaro Epifani <epifanilautaro@gmail.com>
2024-02-08 17:24:02 +0100

    vercel deployment

--- dist/app.js
+++ dist/app.js
@@ -12,6 +12,19 @@ const port = process.env.PORT;
 app.use(express_1.default.json()); // Middleware para parsear JSON
 app.use((0, cors_1.default)());
 // Rutas
+app.get("/", (req, res) => {
+    const htmlResponse = `
+      <html>
+        <head>
+          <title>NodeJs y Express en Vercel</title>
+        </head>
+        <body>
+          <h1>Soy un proyecto Back end en vercel</h1>
+        </body>
+      </html>
+    `;
+    res.send(htmlResponse);
+});
 app.use('/api/payments', paymentRoute_1.default);
 app.options('*', (0, cors_1.default)());
 app.listen(port, () => {
